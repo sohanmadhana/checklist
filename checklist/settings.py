8ad4510c0aae0e1037687911025ad76e88186e14
Sohan Madhana <54635473+sohanmadhana@users.noreply.github.com>
2021-09-03 17:49:55 +0530

    Update settings.py

--- checklist/settings.py
+++ checklist/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django-heroku
+import dj_database_url
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -164,3 +165,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 # print(os.getcwd())
 print(BASE_DIR)
 django_heroku.settings(locals())
+
+
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
